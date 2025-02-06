33635ad0040f1ba19a1666498438d74c09a496d5
SVARGus <kyznetsovpn@mail.ru>
2025-02-06 23:30:51 +0300

    Частично реализованы классы Book, BookResurs

---
+++ Class_Work_31_01_2025_Library/Clases/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_and_Class_Work_31_01_2025_Library.Clases
+{
+    public class Book
+    {
+        int Id { get; set; } // уникальный номер книги
+        public string Title { get; set; } // Название книги
+        public string Author { get; set; } // Автор книги
+        public string Genre { get; set; } // Жанр книги 
+        public string Publisher { get; set; } // Издатель
+        public bool IsAvailable { get; set; } // Доступность книги
+        public int TotalCopies { get; set; } // общее количество экземпляров
+        public int AvailableCopies { get; set; } // Сколько сейчас доступно книг
+        public string Description { get; set; } // Описание книги, можно запихнуть в подсказки - для отображения
+        public Book() { }
+        public Book(int id, string title, string author, string genre, 
+            string publisher, int totalCopies, int availableCopies, string description)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            Genre = genre;
+            Publisher = publisher;
+            TotalCopies = totalCopies;
+            AvailableCopies = availableCopies;
+            Description = description;
+            if(AvailableCopies == 0)
+                IsAvailable = false;
+            else 
+                IsAvailable = true;
+        }
+        public override string ToString()
+        {
+            return base.ToString(); // подумать что будет отображаться в списке книг
+        }
+    }
+}
---
+++ Class_Work_31_01_2025_Library/Data/BookResurs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Class_Work_31_01_2025_Library.Clases;
+using Home_and_Class_Work_31_01_2025_Library.Clases;
+
+namespace Home_and_Class_Work_31_01_2025_Library.Data
+{
+    public static class BookResurs
+    {
+        private static List<Book> listBooks;
+        private static string FilePath = "libraryBooks.json";
+        public static List<Book> GetListBooks()
+        {
+            if (listBooks == null)
+            {
+                LoadListBooks();
+                if (listBooks == null)
+                {
+                    listBooks = new List<Book>();
+                }
+            }
+            return listBooks;
+        }
+        private static void LoadListBooks(string filePath = null)
+        {
+            if (filePath == null)
+                filePath = FilePath;
+
+        }
+    }
+}
